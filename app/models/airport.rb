d103a96864cee70abd3ac7951e54c3b6ab9b9f10
paramagicdev <konnor5456@gmail.com>
2019-05-11 14:25:32 -0400

    removed unncessary associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,14 +1,8 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: 'Flight',
-                               foreign_key: 'from_airport_id',
-                               dependent: :destroy,
-                               source: :from_airport
+                               foreign_key: 'from_airport_id'
 
   has_many :arriving_flights, class_name: 'Flight',
-                              foreign_key: 'to_airport_id',
-                              dependent: :destroy,
-                              source: :to_airport
+                              foreign_key: 'to_airport_id'
 
-  # has_many :arriving_flights, through: :arriving_flights, source: :to_airport
-  # has_many :departing_flights, through: :departing_flights, source: :from_airport
 end
