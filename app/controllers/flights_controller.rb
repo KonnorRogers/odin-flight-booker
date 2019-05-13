b1b47b31502dc9ad6cc10a0744ae420943f6a3ab
paramagicdev <konnor5456@gmail.com>
2019-05-12 21:17:02 -0400

    added working dropdowns for airports and added routes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    # Creates an array of arrays for use by options for select
+    @airport_options = Airport.all.map { |a| [ a.name, a.id ] }
+    @flight = Flight.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'flights#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :flights, only: %i[index create]
 end
