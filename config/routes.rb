3f42f654ff267a9580e3d3b27d2b71ed3f390042
paramagicdev <konnor5456@gmail.com>
2019-05-26 15:31:30 -0400

    regenerated BookingsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookings_controller/new'
-  get 'bookings_controller/create'
-  get 'bookings_controller/show'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/show'
   root 'flights#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flights, only: %i[index create]
--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
-class BookingsControllerControllerTest < ActionDispatch::IntegrationTest
+class BookingsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get bookings_controller_new_url
+    get bookings_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get bookings_controller_create_url
+    get bookings_create_url
     assert_response :success
   end
 
   test "should get show" do
-    get bookings_controller_show_url
+    get bookings_show_url
     assert_response :success
   end
 
