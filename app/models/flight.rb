5ae58081683b64c010e0dccb770bbbbc98c5c7d6
paramagicdev <konnor5456@gmail.com>
2019-05-26 15:43:24 -0400

    fixing associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,9 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: 'Airport'
   belongs_to :to_airport, class_name: 'Airport'
+
   has_many :bookings
+  has_many :flights, through: :bookings
 
   validates :from_airport, presence: true
   validates :to_airport, presence: true
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,5 @@
 class Passenger < ApplicationRecord
   belongs_to :bookings
-  # uses a through model because the booking contains the info
-  # for the flight
   has_many :flights, through: :bookings
 
   validates :name, presence: true
