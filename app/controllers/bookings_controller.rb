4e8e564263412b132adc180ae636ef018a070532
paramagicdev <konnor5456@gmail.com>
2019-05-26 15:38:44 -0400

    handling bookings, on step #5

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,21 @@
 class BookingsController < ApplicationController
   def new
+    @flight_id = params[:flight].id
+    @num_passengers = params[:num_passengers]
+
+    @passengers = []
+    @num_passenger.times { @passengers << Passenger.new }
   end
 
   def create
   end
 
   def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight, :num_passengers)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
   root 'flights#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flights, only: %i[index create]
+  resources :bookings, only: %i[new create show]
 end
