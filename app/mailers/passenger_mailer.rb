e81aa6b31fbdb8f6ae74889d7ac82d6e644ac9fe
paramagicdev <konnor5456@gmail.com>
2019-06-06 15:31:02 -0400

    Add thank_you_email views and setup letter_opener

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'konnor@example.com'
+
+  def thank_you_email
+    # @user = params[:user]
+    # @url = 'http://example.com'
+    # mail(to: @user.email, subject: "Thank you for purchasing your ticket")
+  end
 end
